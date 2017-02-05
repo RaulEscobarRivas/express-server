ee1c039bde0c940a2cc7a32e82a01ca303b89098
Raul Escobar <raulescobar13@gmail.com>
2017-02-04 21:52:07 -0300

    Writes a hook on user model save method so it encrypts the password before saving to the database

--- models/user.js
+++ models/user.js
@@ -1,10 +1,27 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
 const Schema = mongoose.Schema;
 
 // Defines model
 const userSchema = new Schema({
     email: { type: String, unique: true, lowercase: true },
     password: String
+})
+
+// Hooks to Save method, encrypts password
+userSchema.pre('save', function(next) {
+    const user = this;
+
+    bcrypt.genSalt(10, function(err, salt) {
+        if (err) { return next(err); }
+
+        bcrypt.hash(user.password, salt, null, function(err, hash) {
+            if (err) { return next(err); }
+
+            user.password = hash;
+            next();
+        })
+    });
 })
 
 // Create the model class
