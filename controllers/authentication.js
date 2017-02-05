27c7290139bdcf1c8202d46306333a67d56678fc
Raul Escobar <raulescobar13@gmail.com>
2017-02-04 21:26:18 -0300

    Implements authentication controller

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -4,7 +4,24 @@ exports.signup = function(request, response, next) {
     const email = request.body.email;
     const password = request.body.password;
 
-    User.findOne({ email: email }, function(err, existingUser){
+    console.log(request.body);
 
+    User.findOne({ email: email }, function(err, existingUser){
+        if (err) { return next(err); }
+
+        if (existingUser) {
+            response.status(422).send({ error: 'Email already in use.'});
+        }
+
+        const user = new User({
+            email: email,
+            password: password
+        })
+
+        user.save(function(err) {
+            if (err) { return next(err); }
+
+            response.json({ succes: true });
+        });
     });
 }
